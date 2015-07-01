406a9a026f30885a5369a547bd5c3e6eab621fdb
Robert Lester <robertlesterjr@mac.com>
2015-07-01 14:25:32 -0400

    Added update badging to menu. Started the install feature.

--- cp-config.php
+++ cp-config.php
@@ -8,7 +8,7 @@ define('DB_SERVER', 'localhost');
 define('DB_NAME', 'control_panel');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
-define('DB_PREFIX', 'my_');
+define('DB_PREFIX', 'cp_');
 define('DB_PORT', '8889');
 
 define('CP_WORKING_DIR', __DIR__ );
