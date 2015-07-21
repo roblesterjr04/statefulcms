7a7044a5908c0cca7e84952c85c845932da826df
Robert Lester <robertlesterjr@mac.com>
2015-07-20 21:16:09 -0400

    state.php added comments.

--- admin/state.php
+++ admin/state.php
@@ -11,26 +11,24 @@ require_once '../core/init.php';
 
 $state_return = true;
 
+# Collect the data from the state request.
+
 $object_coded = isset($_REQUEST['object']) ? $_REQUEST['object'] : false;
 $data = isset($_REQUEST['data']) ? $_REQUEST['data'] : false;
-
 $callback = isset($_REQUEST['callback']) ? $_REQUEST['callback'] : false;
-
 $event = isset($_REQUEST['event']) ? $_REQUEST['event'] : false;
+$sender_in = isset($_REQUEST['sender']) ? $_REQUEST['sender'] : false;
 
-$object = root()->decode($object_coded);
+$object = root()->decode($object_coded); # Decode the object.
+$sender = root()->decode($sender_in); # Decode the sender.
 
 $slug = $object->_slug;
 
-echo "{$slug}_sessionState = '$object_coded';\n";
+echo "{$slug}_sessionState = '$object_coded';\n"; # Output the javascript to the browser with the object we are working with. The encoded object.
 
-$func = $callback . '_' . $event;
+$func = $callback . '_' . $event; # Define the function we are going to call out of our object.
 
-$sender_in = isset($_REQUEST['sender']) ? $_REQUEST['sender'] : false;
-
-$sender = root()->decode($sender_in);
-
-// We can't change yet, we need to update the state with our changed value, and once the new state is saved. We can call the change callback.
+# We can't change yet, we need to update the state with our changed value, and once the new state is saved. We can call the change callback.
 if ($event == '_change' || $event == '_keyup') {
 	if (get_class($sender) == 'CP_Editor') {
 		echo "var newval = CKEDITOR.instances.$callback.getData();";
@@ -39,17 +37,17 @@ if ($event == '_change' || $event == '_keyup') {
 	} else {
 		echo "var newval = $('*[name=\"$callback\"]').val();\n";
 	}
-	//echo "console.log('Executing state update: '+newval);\n";
 	root()->iface->console("Initiating state update...");
 	echo "cp_state('$callback', {$slug}_sessionState, '$sender_in', 'update_state', newval, true, '".substr($event,1)."', '$slug');\n\n";
 }
 
-// We are being asked to update the state. 
+# We are being asked to update the state. 
 else if ($event == 'update_state') {
 	root()->iface->console("Updating the state...");
 	$object->controls->$callback->val($data, false);
 }
 
+# We don't have anything else do do, so lets just execute the callback function.
 else {
 
 	$sender = root()->decode($sender_in);
